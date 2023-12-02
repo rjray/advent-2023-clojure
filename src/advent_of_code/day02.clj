0d609802812d3ce33cd6fcd3821222537200823a
Randy J. Ray <rjray@blackperl.com>
2023-12-02 09:47:25 -0700

    Commented code, removed an unused param to part-1

--- src/advent_of_code/day02.clj
+++ src/advent_of_code/day02.clj
@@ -2,6 +2,9 @@
   (:require [advent-of-code.utils :as u]
             [clojure.string :as str]))
 
+;; These three defn's handle parsing the data. The last one could have been
+;; skipped, and just had the part-1/part-2 functions directly use `map` +
+;; `parse-one-line`.
 (defn- parse-round [round]
   (let [round (str/replace round "," "")
         parts (partition 2 (str/split round #"\s+"))]
@@ -18,26 +21,33 @@
 (defn- parse-games [lines]
   (map parse-one-line lines))
 
+;; Determine if a round is not possible given the counts of r, g, b
 (defn- not-possible [r g b round]
   (or (< r (get round :red 0))
       (< g (get round :green 0))
       (< b (get round :blue 0))))
 
+;; Determine if a game is possible by checking to see if any rounds are not
+;; possible.
 (defn- is-possible? [r g b {rounds :rounds}]
   (not (some #(not-possible r g b %) rounds)))
 
+;; Filter the full set of games by the `is-possible?` predicate, then pull out
+;; their ID values.
 (defn- possible-games [r g b games]
   (map :game (filter #(is-possible? r g b %) games)))
 
 (defn part-1
   "Day 02 Part 1"
-  [input & vals]
+  [input]
   (->> input
        u/to-lines
        parse-games
        (possible-games 12 13 14)
        (reduce +)))
 
+;; Much easier than part 1. Just use `apply` + `max` on each of the three
+;; colors and return this as a list.
 (defn- get-min-needed [{rounds :rounds}]
   (list (apply max (map #(get % :red 0) rounds))
         (apply max (map #(get % :green 0) rounds))
