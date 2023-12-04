ea9c274ef5bc50458a7c15ce5e5a38f695434e1a
Randy J. Ray <rjray@blackperl.com>
2023-12-04 09:28:46 -0700

    Added some comments

--- src/advent_of_code/day04.clj
+++ src/advent_of_code/day04.clj
@@ -2,12 +2,18 @@
   (:require [advent-of-code.utils :as u]
             [clojure.string :as str]))
 
+;; Parse a single card. We need to separate groups of numbers, so first split
+;; on the "|". Then use parse-out-longs from utils. The winning numbers will be
+;; expressed as a set for easy testing, "my" numbers will be left as a list.
 (defn- parse-card [card]
   (let [[winning mine] (str/split card #"\s+[|]\s+")
         w'             (u/parse-out-longs winning)
         m'             (u/parse-out-longs mine)]
     [(set (rest w')) m']))
 
+;; Determine the "score" of a card-- how many of my numbers match the winning
+;; numbers. Don't worry about converting that to points, we'll do that in the
+;; main loop.
 (defn- score-card [[winning mine]]
   (count (filter winning mine)))
 
@@ -23,6 +29,9 @@
        (map #(reduce * (repeat % 2)))
        (reduce +)))
 
+;; Adjust the vector of `counts` based on how many winning numbers card `n`
+;; had. Note that each "prize" card is increased by `x`, where x is the number
+;; of instances of card n that we have.
 (defn- adjust [counts n wins]
   (let [x (counts n)]
     (loop [[m & ms] (range wins), counts counts]
@@ -30,6 +39,8 @@
         (nil? m) counts
         :else    (recur ms (update counts (+ n m 1) + x))))))
 
+;; Count the cards. Start at the first card and go forward. No need for any
+;; backtracking due to the nature of the problem (all additions are forward).
 (defn- count-cards [cards]
   (let [counts (vec (repeat (count cards) 1))]
     (loop [[n & ns] (range (count cards)), counts counts]
