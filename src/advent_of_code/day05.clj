d90e3655742dd84f9afc3010a8e41e8e19043b9c
Randy J. Ray <rjray@blackperl.com>
2023-12-05 21:42:48 -0800

    Added comments

--- src/advent_of_code/day05.clj
+++ src/advent_of_code/day05.clj
@@ -2,6 +2,8 @@
   (:require [advent-of-code.utils :as u]
             [clojure.string :as str]))
 
+;; Create the block for a single mapping. Takes the "name" and converts it to a
+;; keyword, then massages the numbers into something I can use, in map form.
 (defn- create-block [[label & lines]]
   (let [label (keyword (first (str/split label #"\s+")))]
     (list label
@@ -10,35 +12,50 @@
                       (assoc mapping from (list to length))))
                   {} lines))))
 
+;; Parse one block. The blocks were created by u/to-blocks, this will handle a
+;; single one by breaking it into lines and calling the above defn.
 (defn- parse-block [block]
   (->> block
        u/to-lines
        create-block))
 
+;; Create the 7 x-to-y maps. It turns out that this is over-engineered, as I
+;; didn't look at the input file closely-enough to realize the maps were already
+;; in the order they would be used. There was no reason to store the map "name"
+;; with each one.
 (defn- create-maps [[seeds & blocks]]
   (let [seeds  (u/parse-out-longs seeds)
         blocks (map parse-block blocks)]
     (assoc (into {} (map hash-map (map first blocks) (map last blocks)))
            :seeds seeds)))
 
+;; Get the value for the seed `val`, using the values from a mapping.
 (defn- get-val [base [off len] val]
   (if (>= val (+ base len)) val (+ (- val base) off)))
 
+;; Find a value in a given mapping, using the rules given. `fr` and `to` are
+;; used to create the keyword that indexes the map, but it turns out that this
+;; wasn't necessary after all. I'll probably refactor this code later to remove
+;; this.
 (defn- find-in-mapping [val fr to maps]
   (let [this-map-key (keyword (apply str (list fr "-to-" to)))
         this-map     (maps this-map-key)
         base         (last (take-while #(<= % val) (sort (keys this-map))))]
     (if base (get-val base (this-map base) val) val)))
 
+;; This creates the pairs that specify each map. The 8 "elements" are in order,
+;; and the `(partition 2 1 ...)` creates all the adjacent pairs.
 (def ^:private pairs
   (partition 2 1 ["seed" "soil" "fertilizer" "water" "light" "temperature"
                   "humidity" "location"]))
 
+;; Using `find-in-mapping` and `pairs`, get the location for a given `seed`.
 (defn- find-location [maps seed]
   (reduce (fn [val pair]
             (find-in-mapping val (first pair) (last pair) maps))
           seed pairs))
 
+;; Find the locations for all seeds read from the input data.
 (defn- find-locations [maps]
   (map #(find-location maps %) (:seeds maps)))
 
@@ -51,15 +68,9 @@
        find-locations
        (reduce min)))
 
-;; These didn't work. Brute force is not viable here.
-;; (defn- find-location-by-range [maps [base len]]
-;;   (apply min (map #(find-location maps %) (range base (+ base len)))))
-
-;; (defn- find-locations-by-range [maps]
-;;   (map #(find-location-by-range maps %) (partition 2 (:seeds maps))))
-
-;; Basing this on the Python code at:
-;; https://github.com/Leftfish/Advent-of-Code-2023/blob/main/05/d05.py
+;; Find the smallest location in the given seed-range, using sampling based on
+;; the value of `step`. I realize now, looking at it, that I could do the
+;; management of `newval` on lines 79-81 better.
 (defn- find-smallest-in-range [rng maps step]
   (loop [[current & cs] (range (first rng) (apply + rng) step)
          minimum        [-1 -1]]
@@ -71,11 +82,16 @@
           (recur cs [newval current])
           (recur cs minimum))))))
 
+;; This is a quick-and-dirty "copy" of Python's ability to use `<` on tuples.
 (defn- our< [[t11 t12] [t21 t22]]
   (if (not= t11 t21)
     (< t11 t21)
     (< t12 t22)))
 
+;; Find the smallest location over the set of ranges, based on taking the
+;; numbers in `:seeds` pair-wise. Note that because of the huge values of
+;; `step` and `stop-step`, this can't be run on the example input from the
+;; puzzle page.
 (defn- find-smallest-in-ranges [maps]
   (let [seed-ranges (partition 2 (:seeds maps))
         stop-step   10000]
