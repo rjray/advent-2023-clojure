68c2366d07f429ce018acc974eb6a2c051321481
Randy J. Ray <rjray@blackperl.com>
2023-12-09 10:14:14 -0800

    Commenting Day 9

--- src/advent_of_code/day09.clj
+++ src/advent_of_code/day09.clj
@@ -1,6 +1,9 @@
 (ns advent-of-code.day09
   (:require [advent-of-code.utils :as u]))
 
+;; This is all the logic needed for both parts 1 and 2. Start with the input
+;; sequence and do a step-wise reduction until we reach a sequence of all 0.
+;; Along the way, sum all of the end (`last`) numbers.
 (defn- extrapolate [sequence]
   (loop [sequence sequence, next-term 0]
     (if (every? zero? sequence)
@@ -17,6 +20,8 @@
        (map extrapolate)
        (reduce +)))
 
+;; For part 2, we just have to reverse all of the lines and `extrapolate` will
+;; still work.
 (defn part-2
   "Day 09 Part 2"
   [input]
