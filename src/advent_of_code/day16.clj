d44e094c0b857ccaedeb58a4de87f8955329fba4
Randy J. Ray <rjray@blackperl.com>
2023-12-16 11:27:42 -0800

    Comments on Day 16

--- src/advent_of_code/day16.clj
+++ src/advent_of_code/day16.clj
@@ -1,11 +1,17 @@
 (ns advent-of-code.day16
   (:require [advent-of-code.utils :as u]))
 
+;; This is used for easy changing of position based on the beam's direction.
 (def ^:private delta {:n [-1 0]
                       :e [0 1]
                       :s [1 0]
                       :w [0 -1]})
 
+;; This function takes the beam's position, direction, and the character it is
+;; currently on. Based on the character and direction, it returns a new beam
+;; representation (or two, if `ch` is one of the splitters). I could probably
+;; do this more tightly with a mapping or two, or if there's a Clojure
+;; equivalent of Rust's pattern matching that I don't yet know.
 (defn- process-beam [pos dir ch]
   (case ch
     \. (list [pos dir])
@@ -26,12 +32,20 @@
          :e (list [pos :n])
          :w (list [pos :s]))))
 
+;; Trace a single beam from the initial point/direction. Count the number of
+;; points in `matrix` that get charged. Note that `initial` starts OUTSIDE the
+;; field, as the first thing done is to update the position.
 (defn- trace [initial matrix]
+  ;; Make a local `valid?` predicate based on the size of `matrix`.
   (letfn [(valid? [[y x]]
             (and (>= x 0)
                  (>= y 0)
                  (< x (count (first matrix)))
                  (< y (count matrix))))]
+    ;; The sequence of "beams" will not be empty until the last beam has gone
+    ;; off the edge of the field. The `seen` set tracks each time a point is
+    ;; entered from each direction, since a second beam following a previous
+    ;; path isn't going to change the count.
     (loop [[beam & beams] (list initial), seen #{}, charged #{}]
       (if (nil? beam)
         (count charged)
@@ -51,9 +65,15 @@
        u/to-matrix
        (trace [[0 -1] :e])))
 
+;; For part 2, this traces all beams from each edge-point going inward to the
+;; field. This means that corner points like [0 0] are tested twice, once for
+;; each direction.
 (defn- trace-all [matrix]
   (let [max-y (count matrix)
         max-x (count (first matrix))]
+    ;; I suspect there was probably a clever way to do this with just one `for`
+    ;; block, using nested y/x ranges. But this was easier to do in a short
+    ;; time.
     (flatten
      (concat
       (for [y (range max-y)]
