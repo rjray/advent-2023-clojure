58b1c4e55ea08a41b569272a9f881e9d237a0ab4
Randy J. Ray <rjray@blackperl.com>
2023-12-18 17:06:07 -0800

    Small enhancement to Day 16
    Just uses a hash-map to look up directional changes for mirrors in the
    `process-beam` function. Shortens the code by about 8-10 lines and
    brings in a tiny speed-up.

---
+++ src/advent_of_code/day16bis.clj
@@ -0,0 +1,82 @@
+(ns advent-of-code.day16bis
+  (:require [advent-of-code.utils :as u]))
+
+;; This is used for easy changing of position based on the beam's direction.
+(def ^:private delta {:n [-1 0]
+                      :e [0 1]
+                      :s [1 0]
+                      :w [0 -1]})
+
+(def ^:private mirror-map {\\ {:n :w, :s :e, :e :s, :w :n}
+                           \/ {:n :e, :s :w, :e :n, :w :s}})
+
+;; This function takes the beam's position, direction, and the character it is
+;; currently on. Based on the character and direction, it returns a new beam
+;; representation (or two, if `ch` is one of the splitters).
+(defn- process-beam [pos dir ch]
+  (case ch
+    \. (list [pos dir])
+    \- (case dir
+         (:e :w) (list [pos dir])
+         (:n :s) (list [pos :e] [pos :w]))
+    \| (case dir
+         (:n :s) (list [pos dir])
+         (:w :e) (list [pos :n] [pos :s]))
+    (list [pos (get-in mirror-map [ch dir])])))
+
+;; Trace a single beam from the initial point/direction. Count the number of
+;; points in `matrix` that get charged. Note that `initial` starts OUTSIDE the
+;; field, as the first thing done is to update the position.
+(defn- trace [initial matrix]
+  ;; Make a local `valid?` predicate based on the size of `matrix`.
+  (letfn [(valid? [[y x]]
+            (and (>= x 0)
+                 (>= y 0)
+                 (< x (count (first matrix)))
+                 (< y (count matrix))))]
+    ;; The sequence of "beams" will not be empty until the last beam has gone
+    ;; off the edge of the field. The `seen` set tracks each time a point is
+    ;; entered from each direction, since a second beam following a previous
+    ;; path isn't going to change the count.
+    (loop [[beam & beams] (list initial), seen #{}, charged #{}]
+      (if (nil? beam)
+        (count charged)
+        (let [[pos dir] beam
+              newpos    (mapv + pos (delta dir))]
+          (if (and (valid? newpos) (not (seen (list newpos dir))))
+            (recur (concat beams (process-beam newpos dir
+                                               (get-in matrix newpos)))
+                   (conj seen (list newpos dir))
+                   (conj charged newpos))
+            (recur beams seen charged)))))))
+
+(defn part-1
+  "Day 16 Part 1"
+  [input]
+  (->> input
+       u/to-matrix
+       (trace [[0 -1] :e])))
+
+;; For part 2, this traces all beams from each edge-point going inward to the
+;; field. This means that corner points like [0 0] are tested twice, once for
+;; each direction.
+(defn- trace-all [matrix]
+  (let [max-y (count matrix)
+        max-x (count (first matrix))]
+    ;; I suspect there was probably a clever way to do this with just one `for`
+    ;; block, using nested y/x ranges. But this was easier to do in a short
+    ;; time.
+    (flatten
+     (concat
+      (for [y (range max-y)]
+        (list (trace [[y -1] :e] matrix) (trace [[y max-x] :w] matrix)))
+      (for [x (range max-x)]
+        (list (trace [[-1 x] :s] matrix) (trace [[max-y x] :n] matrix)))))))
+
+(defn part-2
+  "Day 16 Part 2"
+  [input]
+  (->> input
+       u/to-matrix
+       trace-all
+       (apply max)))
