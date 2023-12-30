c0c8e6d15cf1ee9750bdff06a1d8f861957eee0c
Randy J. Ray <rjray@blackperl.com>
2023-12-29 19:53:35 -0600

    Commented the Perl algorithm code

--- src/advent_of_code/day25bis.clj
+++ src/advent_of_code/day25bis.clj
@@ -93,6 +93,13 @@
        karger-min-cut
        get-answer))
 
+;; This version of the solution comes from the Perl code that I found through
+;; https://www.reddit.com/r/adventofcode/comments/18qqmfb/2023_day_25_want_to_see_how_your_algorithm_scales/
+;; on reddit. From there, I went to the author's comment on the day's solution
+;; thread: https://www.reddit.com/r/adventofcode/comments/18qbsxs/2023_day_25_solutions/keug4yl/
+;; That comment has the link to the Perl code.
+
+;; Slight change to the previous `to-graph`, to also return the first node.
 (defn- to-graph2 [lines]
   (list (first (str/split (first lines) #":" 2))
         (update-vals
@@ -105,6 +112,8 @@
                  {} (map #(str/split % #"\W+") lines))
          set)))
 
+;; Iterate over the nodes by using a queue updated with each node's neighbors.
+;; Return the count of nodes, the last node, and the initial positions.
 (defn- mark-and-place [first-node graph]
   (loop [queue (list first-node), positions {}, count 0, last-node nil]
     (if-let [node (first queue)]
@@ -116,6 +125,11 @@
                node))
       (list count last-node positions))))
 
+;; This is the inner foreach loop from the `findCut` function in the Perl code.
+;; It iterates over the neighbors of `node`, calculating the average distance
+;; to `node` itself and noting the largest gap (and the edge that has that gap).
+;; Returns a tuple of the avg distance, maximum gap and maximum edge. The
+;; latter two values will be passed into later calls here.
 (defn- neighbors [graph pos node max-gap max-edge last-node]
   (let [neighbors (graph node)
         node-pos  (pos node)]
@@ -128,6 +142,10 @@
             (recur ns (+ avg npos) gap (list node n))
             (recur ns (+ avg npos) max-gap max-edge)))))))
 
+;; This is the outer foreach loop from `findCut`. It loops over the nodes of
+;; the graph, making adjustments to positions map `pos`, the movement range
+;; `mvmt`, and `max-gap` and `max-edge`. Returns a tuple of the updated `pos`,
+;; `mvmt`, and the `max-edge` value.
 (defn- adjust [graph pos first-node last-node]
   (loop [[node & nodes] (keys graph), pos pos, mvmt 0, max-gap 0, max-edge ()]
     (cond
@@ -143,16 +161,25 @@
                new-gap
                new-edge)))))
 
+;; The implementation of `findCut` from the Perl code. This finds the edge that
+;; maximizes the gap to its neighbors and returns that (along with the updated
+;; position data, `pos`). Note that having the `mvmt` metric reach or drop
+;; below 0.05 was later replaced with a maximum iteration length of 3 in the
+;; original author's code.
 (defn- find-cut [graph positions first-node last-node]
   (loop [[pos mvmt max-edge] (list positions 100 nil)]
     (if (<= mvmt 0.05)
       (list max-edge pos)
       (recur (adjust graph pos first-node last-node)))))
 
+;; Update `graph` to remove the given edge. Since the graph is bi-directional,
+;; this has to be removed from both ends.
 (defn- delete-edges [graph src dst]
   (assoc (assoc graph src (disj (graph src) dst))
          dst (disj (graph dst) src)))
 
+;; Find a cut of `graph` of size `cuts`. Once found, derive the product
+;; specified by the puzzle.
 (defn- split-graph [cuts [first-node graph]]
   (let [node-count              (count (keys graph))
         [_ last-node positions] (mark-and-place first-node graph)]
